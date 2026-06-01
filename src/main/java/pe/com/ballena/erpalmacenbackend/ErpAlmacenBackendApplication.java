f89a1f99a4aa9a09eed7900056e9bd62f9d65f1d
Rupert Nique <rniquesegura@gmail.com>
2026-06-01 13:23:32 -0500

    iniciaciòn backend

---
+++ src/main/java/pe/com/ballena/erpalmacenbackend/ErpAlmacenBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.com.ballena.erpalmacenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpAlmacenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpAlmacenBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/ballena/erpalmacenbackend/ErpAlmacenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.ballena.erpalmacenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpAlmacenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
